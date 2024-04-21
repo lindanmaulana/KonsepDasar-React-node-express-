06c587d6ee67c44eb772fbde213a37bdf7abf963
dian ardiansyah <you@example.com>
2024-04-22 06:14:23 +0700

    update interface app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
 // import Regist from "./Pages/Regist";
 function App() {
+  const password = useRef(null)
+  const [read, setRead] = useState(false)
+
+  const handleForm = (event) => {
+    event.preventDefault();
+  };
+
+  // handle read password
+  const handlePassword = () => {
+    if(read === false) {
+      setRead(true)
+    } else {
+      setRead(false)
+    }
+  }
+
+  useEffect(() => {
+    (read ? password.current.type = 'text' : password.current.type = 'password')
+  }, [read])
+
   return (
     <>
       {/* <Regist /> */}
@@ -26,7 +48,7 @@ function App() {
             <div className="w-[960px] h-screen flex justify-center items-center">
               <div className="w-[508px] ">
                 <form
-                  action=""
+                  onSubmit={handleForm}
                   className="flex flex-col justify-center items-center gap-y-[30px]"
                 >
                   {/* form title */}
@@ -49,16 +71,19 @@ function App() {
 
                   {/* form input */}
                   <div className="w-[371.22px] flex flex-col gap-y-[18px]  ">
-
                     {/* username */}
                     <div className="w-full h-[45px] flex items-center border border-[#5867DD] rounded-[50px]  ">
+                      <label htmlFor="username">
+
                       <img
                         src="../public/loginUsername.svg"
                         alt="username"
                         className="w-[25px] h-[25px] ml-[22.53px]"
                       />
+                      </label>
                       <input
                         type="text"
+                        id="username"
                         placeholder="Username"
                         className="placeholder:text-[#5867DD] ml-[12px] bg-transparent outline-none "
                       />
@@ -66,13 +91,17 @@ function App() {
 
                     {/* password */}
                     <div className="w-full h-[45px] flex items-center border border-[#5867DD] rounded-[50px]  ">
-                      <img
-                        src="../public/loginPassword.svg"
-                        alt="username"
-                        className="w-[25px] h-[25px] ml-[22.53px]"
-                      />
+                      <label htmlFor="password">
+                        <img
+                          src="../public/loginPassword.svg"
+                          alt="username"
+                          className="w-[25px] h-[25px] ml-[22.53px]"
+                        />
+                      </label>
                       <input
                         type="password"
+                        ref={password}
+                        id="password"
                         placeholder="Password"
                         className="placeholder:text-[#5867DD] ml-[12px] bg-transparent outline-none "
                       />
@@ -80,17 +109,15 @@ function App() {
 
                     {/* remember password */}
                     <div className="w-[120.28px] flex gap-x-[8px] ml-[2px]">
-                      <img
-                        src="../public/loginRemember.svg"
-                        alt="remember"
-                        className="self-center w-3 h-3"
-                      />
-                      <p className="text-[14px] text-[#5867DD]">Remember me</p>
+                      <input type="checkbox" name="" onChange={handlePassword} id="" className="self-center w-3 h-3 mt-[1px]"/>
+                      <p className="self-center text-[14px] text-[#5867DD]">Remember me</p>
                     </div>
 
                     {/* button login */}
                     <div className="w-full h-[45px] flex items-center bg-[#5867DD] rounded-[50px]">
-                      <button className="w-full h-full text-[18px] text-[#FFFFFF]">LOGIN</button>
+                      <button className="w-full h-full text-[18px] text-[#FFFFFF]">
+                        LOGIN
+                      </button>
                     </div>
 
                     {/* forgot password */}
@@ -100,7 +127,9 @@ function App() {
                         alt="remember"
                         className="self-center w-3.5 h-3.5"
                       />
-                      <p className="text-[14px] text-[#5867DD]">Forgot Password?</p>
+                      <p className="text-[14px] text-[#5867DD]">
+                        Forgot Password?
+                      </p>
                     </div>
                   </div>
                 </form>
